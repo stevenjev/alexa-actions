3aeeaf26c1132153500624041255d30d7612d9a7
stevenjev <steven.uwo@gmail.com>
2020-05-09 18:35:19 -0400

    Updated to add 'get_slot_value'

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
-## VERSION 0.3-beta
+## VERSION 0.3-timeintent
 
 import logging
 import urllib3
@@ -10,6 +10,7 @@ from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.utils import get_slot_value #To get slot value
 
 from ask_sdk_model import Response
 
